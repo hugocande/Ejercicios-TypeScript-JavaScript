59d7f2c295290944d60a6ae17757f157197b0396
hugocande <84963771+hugocande@users.noreply.github.com>
2021-06-22 16:42:24 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+/*Ejercicio 12
+Crea un programa que nos diga los números primos entre 1 y 500
+ */
+
+function esPrimo(numero:number){
+    for (let i = 2; i<=Math.sqrt(numero);i++){
+        if(numero%i === 0){
+            return false;
+        }
+    }
+    if (numero === 1) return false;
+    else return true;
+}
+
+let numerosPrimos:number [] = [];
+for (let i = 1; i<=500; i++){
+    if (esPrimo(i)){
+        numerosPrimos.push(i);
+    }
+}
+console.log(numerosPrimos);
