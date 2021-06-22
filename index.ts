63a2d00c5281cfc7386c1963297ad00ca7483378
hugocande <84963771+hugocande@users.noreply.github.com>
2021-06-22 10:50:39 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+
+let numero:number = 10;
+let solucion:number = 1;
+
+//Usando un for:
+
+for (let i = numero; i>1; i--){
+    solucion = solucion * i;
+}
+console.log(solucion);
+
+
+//Usando recursividad
+
+function factorial (numero:number):number{
+    if (numero>1){
+        return factorial(numero-1)*numero;
+    }
+    else return numero;
+}
+console.log(factorial(numero));
