ea0555c71ba7dd728a7303a1727ea395e204c2a2
hugocande <84963771+hugocande@users.noreply.github.com>
2021-06-22 16:20:00 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+/* Crea un programa que genere 10 números en orden aleatorio entre 1 y 10.
+¡Los números no se pueden repetir!
+El ejercicio consiste en conseguir los números del 1 al 10 pero en orden aleatorio.
+Ejemplo de salida: 9 6 2 8 1 3 7 4 5 10
+ */
+
+const minimo:number = 1;
+const maximo:number = 11;
+let contenedor:number [] = []
+
+function numeroRandom (minimo:number, maximo:number) {
+
+    return Math.floor(Math.random() * (maximo - minimo)) + minimo;
+}
+let random:number = numeroRandom(minimo, maximo);
+contenedor.push(random);
+while (contenedor.length<10){
+
+
+    if (contenedor.includes(random)){
+        random = numeroRandom(minimo,maximo);
+    }
+    else contenedor.push(random);
+}
+
+for (let i = 0; i<contenedor.length;i++){
+    console.log(contenedor[i]);
+}
